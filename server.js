d82243c34d43a8452dc72d18e29b295b6550a314
FranMF8 <129801132+FranMF8@users.noreply.github.com>
2024-09-01 19:51:11 -0300

    Fixes CORS issues

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const fs = require('fs');
 const path = require('path');
 app.use(express.json());
+
+app.use(cors());
 
 
 app.get('/getUsers', function(req, res){
